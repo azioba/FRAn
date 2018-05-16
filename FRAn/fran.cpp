c2bcf813e489efbd1b8303cb4cc213bb9a6862c4
Cazio_Sensei <Cazio_Sensei@DESKTOP-L6MCL55>
2018-05-16 14:48:16 +0200

    Switched DipDir and Dip

--- FRAn/fran.cpp
+++ FRAn/fran.cpp
@@ -54,7 +54,7 @@ void FRAn::convertFile()
 	_ui->convertedData->setRowCount(outputData.size());
 	_ui->convertedData->setColumnCount(2);
 	QStringList m_header;
-	m_header << "Dip" << "Dip Direction";
+	m_header  << "Dip Direction" << "Dip";
 	_ui->convertedData->setHorizontalHeaderLabels(m_header);
 	_ui->convertedData->setShowGrid(false);
 	_ui->convertedData->horizontalHeader()->setSectionResizeMode(0, QHeaderView::ResizeMode::Stretch);
@@ -62,8 +62,8 @@ void FRAn::convertFile()
 
 	for (int i = 0; i < outputData.size(); i++)
 	{
-		_ui->convertedData->setItem(i, 0, new QTableWidgetItem(QString::number(outputData[i].dip)));
-		_ui->convertedData->setItem(i, 1, new QTableWidgetItem(QString::number(outputData[i].dipDir)));		
+		_ui->convertedData->setItem(i, 0, new QTableWidgetItem(QString::number(outputData[i].dipDir)));
+		_ui->convertedData->setItem(i, 1, new QTableWidgetItem(QString::number(outputData[i].dip)));		
 	}
 }
 
